a3de508a436e59a944f3b74e0f0b5fbd75525551
Ivanich <deniska090798@gmail.com>
2024-07-17 23:55:15 +0400

    final

---
+++ tests/example.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require("@playwright/test");
+const { email, password, invalidEmail, invalidPassword } = require("../user.js");
+
+test("Успешный вход", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill(email);
+  await page.getByPlaceholder("Пароль").fill(password);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page.locator("h2")).toHaveText("Моё обучение", { timeout: 10000,});
+  await page.screenshot({ path: "./screen/Успешный вход.png" });
+});
+
+test("Не успешный вход", async ({ page }) => {
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill(invalidEmail);
+  await page.getByPlaceholder("Пароль").fill(invalidPassword);
+  await page.getByTestId("login-submit-btn").click();
+  await expect(page.locator("text=Вы ввели неправильно логин или пароль")).toBeVisible();
+  await page.screenshot({ path: "./screen/Не успешный вход.png" });
+});
